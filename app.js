27e98c88c2c161505823432f34baf425c131719a
Courtney Kerr <courtneykerr219@gmail.com>
2024-11-22 02:35:06 -0600

    Created fetch function in JS file

---
+++ app.js
@@ -0,0 +1,19 @@
+let weather = {
+  apiKey: "cea1b5c39f90da85436bec567502aeb5",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+}
